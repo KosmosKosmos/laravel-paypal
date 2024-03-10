ff733baf8f6aa42b19cb67299b93c5ac344f4325
Andreas Kosmowicz <andreas@kosmoskosmos.de>
2024-03-10 22:12:19 +0100

    Update PaymentCaptures.php: No invoice ID needed anymore

--- src/Traits/PayPalAPI/PaymentCaptures.php
+++ src/Traits/PayPalAPI/PaymentCaptures.php
@@ -38,18 +38,22 @@ trait PaymentCaptures
      *
      * @see https://developer.paypal.com/docs/api/payments/v2/#captures_refund
      */
-    public function refundCapturedPayment(string $capture_id, string $invoice_id, float $amount, string $note)
+    public function refundCapturedPayment(string $capture_id, ?string $invoice_id = null, float $amount, string $note)
     {
         $this->apiEndPoint = "v2/payments/captures/{$capture_id}/refund";
 
-        $this->options['json'] = [
-            'amount' => [
+        $this->options['json'] = [];
+        
+        $this->options['json'][’amount'] = 
+            [
                 'value'         => $amount,
                 'currency_code' => $this->currency,
-            ],
-            'invoice_id'    => $invoice_id,
-            'note_to_payer' => $note,
-        ];
+            ];
+        $this->options['json']['note_to_payer'] = $note;
+            
+        if ($invoice_id) {
+            $this->options['json']['invoice_id'] = $invoice_id;
+        }
 
         $this->verb = 'post';
 
